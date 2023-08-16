cc082f5b53741044302440c3cf612cea3e70e733
심우석 <dntjr5741@naver.com>
2023-08-17 00:06:38 +0900

    0817

--- SWEA/List3.sort.py
+++ SWEA/List3.sort.py
@@ -1,8 +1,13 @@
-N = int(input())
-num = list(map(int, input().split()))
-for i in range(N-1):
-    for j in range(N-1):
-        if num[i] > num[i +1]:
-            num[i], num[i+1] = num[i +1] , num[i]
+T = int(input())
+for q in range(1, T+1):
+    N = int(input())
+    num = list(map(int, input().split()))
+    for i in range(N-1):
+        for j in range(N-1):
+            if num[j] > num[j +1]:
+                num[j], num[j+1] = num[j +1] , num[j]
     
-print(num)
+    print(f'#{q}', end = '')
+    for i in num:
+        print(i, end = ' ')
+    print()
