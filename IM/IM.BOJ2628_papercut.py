c3c06ea3d62c289c532d111b0db425a5310b2c13
SimWooSeok <dntjr5741@naver.com>
2023-08-23 17:54:58 +0900

    '0823'

--- IM/IM.BOJ2628_papercut.py
+++ IM/IM.BOJ2628_papercut.py
@@ -6,3 +6,10 @@ arr = [[0] * x for _ in range(y)]
 cut = int(input())
 for i in range(cut):
     cutx, cuty = map(int, input().split())
+    if cutx == 0:
+        arr[cuty].insert(0,[1]*x)
+    elif cuty == 0:
+        arr[cutx].insert(0,[1]*y)
+
+for i in arr:
+    print(i, sep = ' ')
