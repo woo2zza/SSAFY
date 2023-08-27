99c558d6e24ed78397392b367174801ccb9f4429
심우석 <dntjr5741@naver.com>
2023-08-27 19:53:21 +0900

    0827

--- IM/IM.BOJ2309_seven.py
+++ IM/IM.BOJ2309_seven.py
@@ -1,5 +1,5 @@
-import sys
-sys.stdin = open("input.txt","r")
+# import sys
+# sys.stdin = open("input.txt","r")
 
 # lst = []
 # for _ in range(9):
@@ -29,17 +29,35 @@ sys.stdin = open("input.txt","r")
 # for i in lst:
 #     if i != 0:
 #         print(i)
-def solve():
-    N = 9
-    num = sum(lst) - 100
-    for i in range(N-1):
-        for j in range(i +1, N):
-            if lst[i] + lst[j] ==num:
-                return lst[i] , lst[j]
+
+# def solve():
+#     N = 9
+#     num = sum(lst) - 100
+#     for i in range(N-1):
+#         for j in range(i +1, N):
+#             if lst[i] + lst[j] ==num:
+#                 return lst[i] , lst[j]
 
 
-lst = [int(input()) for _ in range(9)]
-n, m = solve()  # 7명의 포함되지 않는 두명 찾기
-for i in sorted(lst):
-    if i != n and i != m:
-        print(i)
+# lst = [int(input()) for _ in range(9)]
+# n, m = solve()  # 7명의 포함되지 않는 두명 찾기
+# for i in sorted(lst):
+#     if i != n and i != m:
+#         print(i)
+
+
+lst = []
+for i in range(9):
+    hight = int(input())
+    lst.append(hight)
+lst.sort()
+for i in range(0, len(lst)-1):
+    for j in range(i+1, len(lst)):
+       if sum(lst) - (lst[i] + lst[j]) == 100:
+           result = lst[i]
+           result2 = lst[j]
+lst.remove(result)
+lst.remove(result2)
+
+for i in lst:
+    print(i)
--- IM/IM.BOJ2798_Blackjack.py
+++ IM/IM.BOJ2798_Blackjack.py
@@ -21,10 +21,10 @@ N, M = map(int, input().split())
 num = list(map(int, input().split()))
 Max = 0
 ret= 0
-for i in range(0, N-2):
-    for j in range(i+1, N-1):
-        for k in range(j+1, N):
-            ret= num[i] + num[j] + num[k]
-            if Max < ret <= M:
-                Max = ret
-print(Max)
+# for i in range(0, N-2):
+#     for j in range(i+1, N-1):
+#         for k in range(j+1, N):
+#             ret= num[i] + num[j] + num[k]
+#             if Max < ret <= M:
+#                 Max = ret
+# print(Max)
