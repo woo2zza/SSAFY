0b9e6fabcbf41fcbfde018f6e05f5fc1c9ecbb68
심우석 <dntjr5741@naver.com>
2023-08-31 00:04:47 +0900

    0831

--- BOJ/BOJ15657.py
+++ BOJ/BOJ15657.py
@@ -1,7 +1,9 @@
 N, M = map(int, input().split())
 br = list(map(int, input().split()))
+br.sort()
 path = [''] * M
 used = [0] * len(br)
+lst = []
 def abc(level):
     if level == M:
         for i in range(len(path)):
@@ -10,6 +12,7 @@ def abc(level):
         return
 
     for i in range(len(br)):
+        if level > 0 and path[level -1] > br[i] : continue
         path[level] = br[i]
         abc(level + 1)
 abc(0)
