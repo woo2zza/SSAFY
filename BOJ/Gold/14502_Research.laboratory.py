c5e95bbf72be4042867a07b3de2ae9688c034266
심우석 <dntjr5741@naver.com>
2023-09-04 00:52:36 +0900

    0904

--- BOJ/Gold/14502_Research.laboratory.py
+++ BOJ/Gold/14502_Research.laboratory.py
@@ -1,16 +1,10 @@
 from collections import deque
 N, M = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(N)]
-
 deq = deque()
 
 directy = [0,0,1,-1, 0]
 directx = [1,-1,0,0, 0]
-
-
-
-
-
 
 def virus(i, j):
     for k in range(5):
@@ -19,8 +13,6 @@ def virus(i, j):
         if dx < 0 or dy < 0 or dx > N-1 or dy > M-1:continue
         if arr[dy][dx] == 1: continue
         arr[dy][dx] = 2
-
-
 
 for i in range(N):
     for j in range(M):
