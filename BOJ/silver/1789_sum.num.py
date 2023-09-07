a851cf336be9370e6b096d101878f72b744a8354
심우석 <dntjr5741@naver.com>
2023-09-08 01:22:32 +0900

    0908

---
+++ BOJ/silver/1789_sum.num.py
@@ -0,0 +1,14 @@
+S= int(input())
+cnt = 0
+Sum = 0
+Flag = 0
+for i in range(S+1):
+    Sum += i
+    cnt += 1
+    if Sum > S:
+        print(cnt-2)
+        break
+    elif Sum == S:
+      
+        print(cnt-1)
+        break
