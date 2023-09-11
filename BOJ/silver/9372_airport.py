592976c8887f395a7ca44120701a2660e56310f0
SimWooSeok <dntjr5741@naver.com>
2023-09-11 16:27:41 +0900

    '0911'

--- BOJ/silver/9372_airport.py
+++ BOJ/silver/9372_airport.py
@@ -1,6 +1,40 @@
-T = int(input())
-for q in range(T):
-    N, M = map(int, input().split())
-    for i in range(M):
-        a, b = map(int, input().split())
-    print(N-1)
+# import sys
+# input = sys.stdin.readline
+# T = int(input())
+# for q in range(T):
+#     N, M = map(int, input().split())
+#     for i in range(M):
+#         a, b = map(int, input().split())
+#     print(N-1)
+
+def find(x):
+    global parent
+    if parent[x] == -1:
+        return x
+    
+    ret = find(parent[x])
+    parent[x] = ret
+    return ret
+
+
+def union(a, b):
+    
+    global parent, cnt
+    finda, findb = find(a), find(b)
+    if finda == findb:
+        return 
+    else:
+        parent[finda] = findb
+    
+
+N, M = map(int, input().split())
+parent = [-1] *(N+1)
+for i in range(M):
+    first, a, b = map(int, input().split())
+    if first == 0:
+        union(a, b)
+    else:
+        if find(a) == find(b):
+            print('YES')
+        else: 
+            print('NO')
