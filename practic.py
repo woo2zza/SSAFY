a01971ff98ba0b3702e48055e4360afdbaebc326
심우석 <dntjr5741@naver.com>
2023-09-23 17:46:14 +0900

    BOJ

--- practic.py
+++ practic.py
@@ -1,15 +1,11 @@
-N = int(input())
-lst = list(map(int, input().split()))
-lst.sort()
-if lst[0] == 1:
-    lst.remove(lst[0])
+num = int(input())
+num1 = int(input())
 
-result = 0
-for i in lst:
-    cnt = 0
-    for j in range(2, i+1):
-        if i % j == 0:
-            cnt += 1
-    if cnt == 1:
-        result += 1
-print(result)
+if num > 0 and num1 > 0: 
+    print(1)
+elif num > 0 and num1 < 0: 
+    print(4)
+elif num < 0 and num1 > 0: 
+    print(2)
+elif num < 0 and num1 < 0: 
+    print(3)
