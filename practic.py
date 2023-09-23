8c7fa6f2ba50c46e64636131674b36e83f1649ce
심우석 <dntjr5741@naver.com>
2023-09-23 17:41:29 +0900

    0923

--- practic.py
+++ practic.py
@@ -1,3 +1,15 @@
-import sys
-sys.stdin = open("input.txt","r")
+N = int(input())
+lst = list(map(int, input().split()))
+lst.sort()
+if lst[0] == 1:
+    lst.remove(lst[0])
 
+result = 0
+for i in lst:
+    cnt = 0
+    for j in range(2, i+1):
+        if i % j == 0:
+            cnt += 1
+    if cnt == 1:
+        result += 1
+print(result)
