c929ff0bcf5269026bf4b27b87be429375d80d91
wooseok <dntjr5741@naver.com>
2023-09-26 18:01:03 +0900

    0909

---
+++ COMFREHENSION/django_crud/articles/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render, redirect
+from .models import Article
+
+# Create your views here.
+def index(request):
+    articles = Article.objects.all()
+    context = {
+        'articles' : articles,
+    }
+
+    return render(request, 'articles/index.html', context)
+
+def detail(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article' : article,
+    }
+    return render(request, 'articles/detail.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+    # 1
+    # article = Article()
+    # article.title = request.GET.get('title')
+    # article.content = request.GET.get('content')
+    # article.save()
+
+    # 2
+    article = Article(title=title, content=content)
+    article.save()
+
+    # 3
+    # Article.object.create(title=title, content=content)
+
+    return redirect('articles:index')
+
+def delete(request, pk):
+    # 몇 번째 게시글을 삭제할 것인지 조회
+    article = Article.objects.get(pk=pk)
+    # 조회한 게시글을 삭제
+    article.delete()
+    return redirect('articles:inedex')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {
+        'article': article,
+    }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    title = request.POST.get('title')
+    content = request.POST.get('content')
+
+    # 수정하고자 하는 게시글을 조회
+    article = Article.objects.get(pk=pk)
+    article.title = title
+    article.content = content
+    article.save()
+
+    return redirect('articles:detail', article.pk)
+
